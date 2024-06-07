7cff34709d18aba62ddf400c50b6cdb43b508b73
Vinicius Uliano <vgarciauliano@gmail.com>
2024-06-07 15:18:31 -0300

    buscando usuarios

--- src/App.js
+++ src/App.js
@@ -1,12 +1,15 @@
 import './app.css'
 import { useState } from 'react';
 import {db} from './firestoreTeste'
-import { addDoc, collection } from 'firebase/firestore';
+import { addDoc, collection, getDocs } from 'firebase/firestore';
 function App() {
   const [nome, setNome] = useState('')
   const [idade, setIdade] = useState('')
   const [cargo, setCargo] = useState('')
   const [salario, setSalario] = useState('')
+  const [users , setUsers] = useState([])
+
+
 
   async function cadastrarUser(){
     addDoc(collection(db, "User"),{
@@ -14,7 +17,7 @@ function App() {
       idade:idade,
       cargo:cargo,
       salario:salario,
-    
+      
     })
     .then(()=>{
       alert('USUARIO CADASTRADO')
@@ -27,15 +30,54 @@ function App() {
       alert('ERRO NO CADASTRO' + erro)
     })
   }
+
+  async function buscarUser(){
+    const postRef = collection(db, 'User') // Armazenando quais itens quero buscar dentro do banco e qual collection
+    await getDocs(postRef)
+    .then((snapshot)=>{
+      let lista = [] // criando array vazia
+      // criando forEach para percorrer documentos dentro meu snapshot
+      snapshot.forEach((docs)=>{
+        lista.push({
+          id: docs.id,
+          nome: docs.data().nome, // Passando cada informação que quero pegar
+          idade: docs.data().idade,
+          cargo: docs.data().cargo,
+          salario: docs.data().salario,
+
+        })
+      })      
+    setUsers(lista)
+
+    })
+    .catch((error)=>{
+      alert('ERRO AO REALIZAR BUSCA')
+    })
+    
+  }
   return (
     <div className='container'>
         <div className='formulario'>
-        <input type='text' placeholder='Digite seu nome' onChange={(element) => setNome(element.target.value)}></input>
+        <input type='text' placeholder='Digite seu nome' onChange={(element) => setNome(element.target.value)} required></input>
         <input type='number' placeholder='Digite sua idade' onChange={(element) => setIdade(element.target.value)} ></input>
         <input type='text' placeholder='Digite seu cargo' onChange={(element) => setCargo(element.target.value)}></input>
         <input type='number' placeholder='Digite seu salario' onChange={(element) => setSalario(element.target.value)}></input>
         <button onClick={cadastrarUser}>Cadastro</button>
+        <button onClick={buscarUser}>Buscar</button>
         </div>
+      <ul className='lista'>
+          {users.map((usuarios)=>{
+            return(
+              <li key={usuarios.id}>
+                <span>Usuario: {usuarios.nome}</span><br></br>
+                <span>Idade: {usuarios.idade}</span><br></br>
+                <span>Cargo: {usuarios.cargo}</span><br></br>
+                <span>Salario: {usuarios.salario}</span><br></br>
+              </li>             
+            
+            )
+          })}
+      </ul>
     </div>
   );
 }
